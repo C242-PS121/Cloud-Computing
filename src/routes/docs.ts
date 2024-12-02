8ec80f41faa64d2b8fc10a23b1ee3a9ec6471604
Azis Ramadhan <git@azrd.dev>
2024-12-02 23:25:58 +0700

    Add favicon to docs
    testing integration

--- src/routes/docs.ts
+++ src/routes/docs.ts
@@ -1,11 +1,15 @@
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/bun'
 import { apiReference } from '@scalar/hono-api-reference'
 
 import openapi from '../docs'
+
 const docs = new Hono()
 
+docs.get('/icon', serveStatic({ path: './src/docs/favicon.svg' }))
 docs.get('/', (c) => c.json(openapi))
 docs.get('/docs', apiReference({
+	favicon: '/api/icon',
 	pageTitle: 'Thriftify API Docs',
 	theme: 'elysiajs',
 	spec: { url: '/api' },

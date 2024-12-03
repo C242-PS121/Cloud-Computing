de2f0bc36e92e8d98ecd666eae1c304c52e4f82d
Azis Ramadhan <git@azrd.dev>
2024-12-03 12:28:40 +0700

    minor refactor

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,17 +1,9 @@
 import { Hono } from 'hono'
-import { HTTPException } from 'hono/http-exception'
 import { zValidator } from '@hono/zod-validator'
 
 import { post_login, put_login, del_login } from '../schema/auth'
 import { get_user } from '../helpers/users'
-import {
-	add_refresh_token,
-	delete_refresh_token,
-	gen_access_token,
-	gen_refresh_token,
-	get_refresh_token,
-	verify_refresh_token,
-} from '../helpers/auth'
+import * as h from '../helpers/auth'
 
 const auth = new Hono()
 
@@ -24,11 +16,11 @@ auth.post('/login', zValidator('json', post_login), async (c) => {
 	if (!match) return c.json({ message: 'Wrong credentials' }, 401) // simplify
 
 	const [access_token, refresh_token] = await Promise.all([
-		gen_access_token(account.id),
-		gen_refresh_token(account.id),
+		h.gen_access_token(account.id),
+		h.gen_refresh_token(account.id),
 	])
 
-	await add_refresh_token(refresh_token)
+	await h.add_refresh_token(refresh_token)
 	return c.json(
 		{
 			message: 'Successfully logged in',
@@ -44,13 +36,13 @@ auth.post('/login', zValidator('json', post_login), async (c) => {
 auth.put('/login', zValidator('json', put_login), async (c) => {
 	const { refresh_token } = c.req.valid('json')
 
-	const result = await get_refresh_token(refresh_token)
+	const result = await h.get_refresh_token(refresh_token)
 	if (!result) return c.json({ message: "Invalid token, token doesn't exist" }, 401)
 
-	const valid_token = await verify_refresh_token(result.token)
+	const valid_token = await h.verify_refresh_token(result.token)
 	if (!valid_token) return c.json({ message: 'Invalid token' }, 401)
 
-	const access_token = await gen_access_token(valid_token.sub)
+	const access_token = await h.gen_access_token(valid_token.sub)
 
 	return c.json(
 		{
@@ -66,10 +58,10 @@ auth.put('/login', zValidator('json', put_login), async (c) => {
 auth.delete('/logout', zValidator('json', del_login), async (c) => {
 	const { refresh_token } = c.req.valid('json')
 
-	const valid_token = await verify_refresh_token(refresh_token)
+	const valid_token = await h.verify_refresh_token(refresh_token)
 	if (!valid_token) return c.json({ message: 'Invalid token' }, 401)
 
-	const result = await delete_refresh_token(refresh_token)
+	const result = await h.delete_refresh_token(refresh_token)
 	if (!result) return c.json({ message: "Invalid token" }, 401)
 
 	return c.json({ message: 'Successfully logged out' })

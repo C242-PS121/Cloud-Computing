527f7dc6458db3df054c4e0f3ef5280961c791b9
Azis Ramadhan <git@azrd.dev>
2024-12-05 12:40:12 +0700

    fix

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, varchar, decimal, boolean } from 'drizzle-orm/pg-core'
+import { pgTable, text, varchar, integer, boolean } from 'drizzle-orm/pg-core'
 
 export const users = pgTable('users', {
 	id: varchar({ length: 36 }).primaryKey(),
@@ -19,7 +19,7 @@ export const products = pgTable('products', {
 		.references(() => users.id, { onDelete: 'cascade' }),
 	img_url: text().notNull(),
 	name: text().notNull(),
-	price: decimal({ precision: 12, scale: 2 }).notNull(),
+	price: integer().notNull(),
 	description: text().notNull(),
 	clothing_type: text().notNull(),
 	sold: boolean().notNull().default(false),

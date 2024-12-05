9f604980e39c2d56dffcca81fc601a627a5a4ca9
Azis Ramadhan <git@azrd.dev>
2024-12-05 13:32:23 +0700

    remove dummy endpoint documentation

--- src/docs/index.ts
+++ src/docs/index.ts
@@ -5,14 +5,12 @@ import {
 
 import login_docs from './auth'
 import auth_config from './auth/auth_config'
-import dummy_docs from './dummy'
 import users_docs from './users'
 
 const definitions = [
 	...auth_config,
 	...users_docs,
 	...login_docs,
-	...dummy_docs,
 ]
 const openAPI = new OpenApiGeneratorV31(definitions).generateDocument({
 	openapi: '3.1.0',
